0fe6dc6c2954dc92d4168a9bfb3027377d66aa0f
Zi Wang (Austin) Ma <e0696896@u.nus.edu>
2021-04-07 17:28:33 +0800

    Test 2

--- ESSD-CA/Controllers/HomeController.cs
+++ ESSD-CA/Controllers/HomeController.cs
@@ -28,11 +28,6 @@ namespace ESSD_CA.Controllers
             return View();
         }
 
-        public IActionResult JustForTest()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
