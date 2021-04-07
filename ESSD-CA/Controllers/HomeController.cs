666c6259fbacdf12e7fd9464443dec16534c503d
Zi Wang (Austin) Ma <e0696896@u.nus.edu>
2021-04-07 17:59:11 +0800

    Aded test2 method in homecontroller

--- ESSD-CA/Controllers/HomeController.cs
+++ ESSD-CA/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace ESSD_CA.Controllers
             return View();
         }
 
+        public IActionResult Test2()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
