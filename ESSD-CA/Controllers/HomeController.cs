799597a055913f8e9418baead97b971bb8928802
Jean-LY <41562995+Jean-LY@users.noreply.github.com>
2021-04-07 18:02:03 +0800

    Revert "Update HomeController.cs"
    This reverts commit a076471072c3f968444a081ff082db4cf171d0a2.

--- ESSD-CA/Controllers/HomeController.cs
+++ ESSD-CA/Controllers/HomeController.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 
 namespace ESSD_CA.Controllers
 {
-    //Hi Jean testing 
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
