bf766e3bf5000b0aed960d5de34f7db956bb3023
AustinMaZW <82086443+AustinMaZW@users.noreply.github.com>
2021-04-07 18:28:44 +0800

    deleting the unnecessary methods

--- ESSD-CA/Controllers/HomeController.cs
+++ ESSD-CA/Controllers/HomeController.cs
@@ -28,15 +28,6 @@ namespace ESSD_CA.Controllers
             return View();
         }
 
-        public IActionResult JustForTest()
-        {
-            return View();
-        }
-
-        public IActionResult Test2()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
