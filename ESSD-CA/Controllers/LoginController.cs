51ba01bfac3ec9031fd491573c3338586f39ad4b
TCK285 <cktang89@gmail.com>
2021-04-10 20:18:30 +0800

    Loginlogout controller login view _layout edit User model

---
+++ ESSD-CA/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESSD_CA.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate (string username, string password)
+        {
+            
+            
+            if (username == null)
+            {
+                ViewData["username"] = username;
+                ViewData["errLogin"] = "Please enter valid username and password.";
+
+                return View("Index");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
