6aa2077c6597b6d5f66e1cfe0f79c733fe5b17f8
AustinMaZW <82086443+AustinMaZW@users.noreply.github.com>
2021-04-12 21:53:03 +0800

    Updated to include ShoppingCart model, ShopGallery View

--- ESSD-CA/Models/Product.cs
+++ ESSD-CA/Models/Product.cs
@@ -9,13 +9,19 @@ namespace ESSD_CA.Models
     public class Product
     {
         [Required]
+        [MaxLength(50)]
         public string Id { get; set; }
         [Required]
+        [MaxLength(30)]
         public string ProductName { get; set; }
+
+        [MaxLength(200)]
         public string ProductDescription { get; set; }
         [Required]
         public double UnitPrice { get; set; }
+        [MaxLength(200)]
         public string DownloadLink { get; set; }
+        [MaxLength(200)]
         public string ImagePath { get; set; }
 
     }
---
+++ ESSD-CA/Models/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESSD_CA.Models
+{
+    public class ShoppingCart
+    {
+        [MaxLength(50)]
+        public string Id { get; set; }
+
+        [MaxLength(50)]
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+        
+        [MaxLength(50)]
+        public string ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+        
+        [Range(1,1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+        public int Count { get; set; }
+        
+        [NotMapped]
+        public double price { get; set; }
+
+
+    }
+}
