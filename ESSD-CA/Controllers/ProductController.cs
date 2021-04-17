0da899611d7f00aa63a705a136e7a009f036b682
yazhen <yazhen94@gmail.com>
2021-04-17 14:45:42 +0800

    added manage products page for admin

--- ESSD-CA/Controllers/ProductController.cs
+++ ESSD-CA/Controllers/ProductController.cs
@@ -1,10 +1,12 @@
 ﻿using ESSD_CA.Db;
 using ESSD_CA.Models;
-using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace ESSD_CA.Controllers
 {
@@ -19,6 +21,7 @@ namespace ESSD_CA.Controllers
         {
             List<Product> products = db.Products.ToList();  //retrieving products from database and putting into a list
 
+            ViewData["Is_ProductMgmt"] = "bold_menu";
             ViewData["products"] = products;    //sending data view ViewData
             return View();
         }
